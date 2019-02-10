28cbe42fc233881d81f0b428ad197d793f975dc9
SOussama <33095587+SOussama@users.noreply.github.com>
2019-02-10 02:47:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8" />
+        <title>Maths-Perfect</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    
+    <div class="navbar-babybusiness">
+ 
+      <a href="index.php" class="navbar-babybusiness-brand">
+         <img src="Images/mathsperfect1.jpg" alt="photo" class="photo"></a>  
+
+      <a href="#cours" class="navbar-babybusiness-item navbar-babybusiness-link"> Matières enseignées </a> 
+      <a href="#localisation" class="navbar-babybusiness-item navbar-babybusiness-link"> Localisation </a> 
+      <a href="#contact" class="navbar-babybusiness-item navbar-babybusiness-link"> Contact </a> 
+         
+    </div>
+</head>
+    
+
+
+<body>
+    <div class="banner" style="background-image: url('Images/notebook.jpg');">
+
+      <div class="banner-content">
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Bienvenue sur la nouvelle plateforme <br><br>en ligne de Maths-Perfect</h2><br/>
+          <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Encadrement et soutien scolaire scientifique <br><br> dispensé par Dr A. Saouli.</h3>
+          <br/><h4>Depuis 1998</h4>
+      </div>
+    </div>
+
+    <div>
+
+      <h1 id="cours" class="professionnel">Les matières enseignées</h1>
+    </div>
+<br/><br/>
+  <div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-4">
+      <div class="card">
+        <a class="card-link"></a>
+        <img src="Images/math.jpg" alt="math" class="avatar card-avatar">
+        <div class="card-title">
+          <h3>Mathématiques</h3>
+        </div>
+      </div>
+      <p>Les cours sont dispensés pour les niveaux suivant : 
+        <ul>
+          <li>
+            Collège (à partir de la 4<sup>ème</sup>)
+          </li>
+          <li>
+            Lycée
+          </li>
+          <li>
+            Supérieur
+          </li>
+        </ul>
+      </p>
+    </div>
+    <div>
+      <div class="card1">
+        <a class="card-link"></a>
+        <img src="Images/physique.jpg" alt="physique" class="avatar card-avatar">
+          <div class="card1-title">
+            <h3>Physique</h3>
+          </div>
+      </div> 
+      <p>Les cours sont dispensés pour les niveaux suivant : 
+        <ul>
+          <li>
+            Collège
+          </li>
+          <li>
+            Lycée
+          </li>
+          <li>
+            Supérieur
+          </li>
+        </ul>
+      </p>
+      </div>
+      <div> 
+      <div class="card2">
+        <a class="card-link"></a>
+        <img src="Images/chimie.jpg" alt="chimie" class="avatar card-avatar">
+        <div class="card-title">
+          <h3>Chimie</h3>
+        </div>
+      </div>
+      <p>Les cours sont dispensés pour les niveaux suivant : 
+        <ul>
+          <li>
+            Collège
+          </li>
+          <li>
+            Lycée
+          </li>
+        </ul>
+      </p>
+    </div>
+  </div>
+</div>
+<br/><br/>
+<div>
+
+      <h1 id="localisation" class="professionnel">Localisation</h1><br/>
+    </div>
+    <div class="professionnel">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2635.9250103300938!2d2.920098444005933!3d47.399167124664075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f01dee859e0523%3A0x6b29e2d69aadc388!2s44+Avenue+du+85%2C+58200+Cosne-Cours-sur-Loire%2C+France!5e0!3m2!1sfr!2sno!4v1549752065116" width="1000" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+
+
+
+    <br>
+    <div>
+      <h1 id="contact" class="professionnel">Contact</h1><br/>
+    </div>
+    <div>
+      <p>Pour toute demande de renseignements, inscription ou rendez-vous, merci de me contacter via les coordonnées suivantes :</p>
+      <p>Mail&nbsp;:&nbsp;<a href="mailto:maths-perfect@gmail.com">maths-perfect@gmail.com</a></p>
+      <p>Tél&nbsp;:&nbsp; 06.03.81.58.04</p>
+    </div>
+
+      
+
+      <br><br><br>
+     
+
+
+
+
+   
+
+<hr width="90%">
+
+
+
+<footer>
+  <div id="footer" class="footer">
+
+    <div class="footer-copyright">
+      Background and subject images designed by Freepik. Icons designed by Flaticon &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Copyright © 2019 Maths-Perfect. Tous droits réservés
+    </div>
+    <div class="footer-copyright">
+      <img src="Images/mathsperfect.jpg" width="100px" alt="mathsperfect">
+      "Parce que tout élève peut prétendre à la perfection"
+    </div>
+  </div>
+</footer>
+        
+</html>
